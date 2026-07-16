a0a348b6ab1564bc6c39519d699faf4bc675d2fe
shiamsharif <shiam.sharif.07@gmail.com>
2026-07-16 22:16:02 +0600

    FIX: Mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,19 @@
+// src/config/db.js
 const mongoose = require('mongoose');
 
+// Connect to MongoDB
 const connectDB = async () => {
   const { MONGODB_URI } = process.env;
 
+  // Check if MONGODB_URI is defined
   if (!MONGODB_URI) {
     throw new Error('MONGODB_URI is not configured');
   }
 
+  // Connect to the database
   await mongoose.connect(MONGODB_URI);
   console.log(`MongoDB connected: ${mongoose.connection.host}`);
 };
 
+// Disconnect from MongoDB
 module.exports = connectDB;
